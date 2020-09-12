36cb9975fc1d3d2459e780d037d79c7927fec81a
KubsGU <kjelonek00@gmail.com>
2020-09-12 17:10:48 +0200

    Cleaning things up

--- app/Table.js
+++ app/Table.js
@@ -1,13 +1,13 @@
 export class Table {
-    constructor(dealersCards, playersCards){
-        this.dealersCards = dealersCards;
-        this.playersCards = playersCards;
-    }
-    
-    showPlayersCard(card) {
-        this.playersCards.appendChild(card.render());
-    }
-    showDealersCard(card) {
-        this.dealersCards.appendChild(card.render());
-    }
+  constructor(dealersCards, playersCards) {
+    this.dealersCards = dealersCards;
+    this.playersCards = playersCards;
+  }
+
+  showPlayersCard(card) {
+    this.playersCards.appendChild(card.render());
+  }
+  showDealersCard(card) {
+    this.dealersCards.appendChild(card.render());
+  }
 }
